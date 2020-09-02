a1b3421f76d41fd65bf63542b7b9f1cdb1dd8cd8
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-09-02 10:26:51 +0900

    A

---
+++ Todo.php
@@ -0,0 +1,18 @@
+<?php
+
+class Todo
+{
+    // コンストラクタ
+    public function __construct()
+    {
+    }
+
+    /**
+     * @param string $title
+     * @param string $due_date
+     */
+    public function post(string $title, string $due_date)
+    {
+    }
+
+}
